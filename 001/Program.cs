6e59d7287e9cf46c74c540ed1e8ba80b85c1bc5a
Gutnik <cipoga@gmail.com>
2023-07-08 13:56:35 +0300

    final

--- 001/Program.cs
+++ 001/Program.cs
@@ -2,4 +2,13 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-Console.WriteLine("Проверка");
+Console.Write("Введите первое число ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+if (firstnumber > secondnumber) Console.WriteLine(firstnumber);
+    else
+    {
+        Console.Write("Максимальное число: ");
+        Console.WriteLine(secondnumber);
+    }
