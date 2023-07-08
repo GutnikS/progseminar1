42615d822aa6f415f7b19d19027a4c331456ce50
Gutnik <cipoga@gmail.com>
2023-07-08 14:15:05 +0300

    final

--- 002/Program.cs
+++ 002/Program.cs
@@ -2,4 +2,14 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
-Console.WriteLine("Hello, World!");
+Console.Write("Введите первое число ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int thirdnumber = Convert.ToInt32(Console.ReadLine());
+int max = firstnumber;
+if (secondnumber > max) max = secondnumber;
+if (thirdnumber > max) max = thirdnumber;
+
+Console.Write($"Максимальное число: {max}");
