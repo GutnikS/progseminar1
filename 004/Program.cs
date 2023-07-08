434de35597d8076cd3bcf3d442582d221c24d5da
Gutnik <cipoga@gmail.com>
2023-07-08 14:33:25 +0300

    final004

--- 004/Program.cs
+++ 004/Program.cs
@@ -1,4 +1,11 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
-Console.WriteLine("Hello, World!");
+Console.Write("Введите число ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+while (index < (firstnumber-1))
+{
+    index = index + 2;
+    Console.Write ($"{index} ");
+}
